e32103a96cf1a0f4d9f3088938e1df98b7387384
Michael Kim <michael.kim.200518@gmail.com>
2021-07-29 15:51:01 +0900

    update

--- tests/SendTransaction.test.ts
+++ tests/SendTransaction.test.ts
@@ -17,3 +17,37 @@ describe ('Test1', () =>
         WK.make();
     });
 });
+
+describe("Test2", () => {
+    before("Wait for the package libsodium to finish loading", () => {
+        sdk.SodiumHelper.assign(new BOASodium());
+        return sdk.SodiumHelper.init();
+    });
+
+    before("Make Key", () => {
+        WK.make();
+    });
+
+    it("Test 2", () => {
+        for (let key of WK._keys) {
+            console.log(key.secret.toString(false));
+            console.log(key.address.toString());
+        }
+    });
+
+    it("Test 3", () => {
+        console.log(WK.NODE2().secret.toString(false));
+        console.log(WK.NODE2().address.toString());
+        console.log(WK.NODE3().secret.toString(false));
+        console.log(WK.NODE3().address.toString());
+        console.log(WK.NODE4().secret.toString(false));
+        console.log(WK.NODE4().address.toString());
+        console.log(WK.NODE5().secret.toString(false));
+        console.log(WK.NODE5().address.toString());
+        console.log(WK.NODE6().secret.toString(false));
+        console.log(WK.NODE6().address.toString());
+        console.log(WK.NODE7().secret.toString(false));
+        console.log(WK.NODE7().address.toString());
+        console.log(WK.CommonsBudget().address.toString());
+    });
+});
